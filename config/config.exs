902767f0b4599b500a594eb70768e2df30fa27bf
Tristan Cacqueray <tdecacqu@redhat.com>
2022-02-01 03:46:14 +0000

    Add Config module

---
+++ config/config.exs
@@ -0,0 +1,12 @@
+import Config
+
+{api_host, api_path} = case System.get_env("ZUUL") do
+  "sf" ->
+    {"ansible.softwarefactory-project.io", "/zuul/api"}
+  nil ->
+    {"dashboard.zuul.ansible.com", "/api/tenant/ansible"}
+end
+
+config :list_jobs,
+  api_host: api_host,
+  api_path: api_path
